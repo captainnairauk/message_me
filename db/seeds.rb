27df8ff28babc0f2d1d214b321c636209717a070
captainnairauk <aniket1995@yahoo.co.in>
2022-12-14 12:07:59 +0530

    Build user resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Aniket", password: "password")
+User.create(username: "Amruta", password: "password")
+User.create(username: "Unnikrishnan", password: "password")
+User.create(username: "Aparna", password: "password")
+User.create(username: "Jimmy", password: "password")
+User.create(username: "Tejpal", password: "password")
+User.create(username: "Rohan", password: "password")
+User.create(username: "Dinesh", password: "password")
+User.create(username: "Prabhat", password: "password")
+User.create(username: "Adnan", password: "password")
+User.create(username: "Chetan", password: "password")
+User.create(username: "Vinesh", password: "password")
+User.create(username: "Manoj", password: "password")
+User.create(username: "Shubham", password: "password")
